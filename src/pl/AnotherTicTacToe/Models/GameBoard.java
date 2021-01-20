adfca3aca1ae5c8d392fd52be09d84b8ca76e12f
Piotr Kowal <piotrkowal2014@gmail.com>
2021-01-20 14:32:01 +0100

    Added constructor and few methods to GameBoard model

--- src/pl/AnotherTicTacToe/Models/GameBoard.java
+++ src/pl/AnotherTicTacToe/Models/GameBoard.java
@@ -1,5 +1,38 @@
 package pl.AnotherTicTacToe.Models;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class GameBoard implements IGameBoard {
+	private int size;
+	private String[][] gameBoard;
+	private List<Integer> availableNums;
+
+	public GameBoard() {
+		this.size = 3;
+		this.gameBoard = new String[size][size];
+
+	}
+
+	public void addAvaiableNum(int newNumber) {
+		this.availableNums.add(newNumber);
+	}
+
+	public List<Integer> getAvailableNums() {
+		return this.availableNums;
+	}
+
+	public boolean isNumAvailable(int num) {
+		return this.availableNums.indexOf(num) == -1;
+	}
+
+	public boolean isAnyNumAvailable() {
+		return !this.availableNums.isEmpty();
+	}
+
+	public void filterOutAvailableNums(int numToFilterOut) {
+		this.availableNums = this.availableNums.stream().filter(num -> num != numToFilterOut)
+				.collect(Collectors.toList());
+	}
 
 }
